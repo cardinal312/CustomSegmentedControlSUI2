d08fe5b37a3d730b9303171dfd2a03fb2c7a2e34
CARD1NAL <cardinal312@github.com>
2024-07-19 11:42:07 +0600

    Initial Commit

---
+++ CustomSegmentedControlSUI/CustomSegmentedControlSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSegmentedControlSUIApp.swift
+//  CustomSegmentedControlSUI
+//
+//  Created by Macbook on 19/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSegmentedControlSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
